c9c323cc2b5848b57aaad945afacc458ae6e38b4
Falko Schumann <falko.schumann@muspellheim.de>
2020-05-31 14:51:04 +0200

    Initial Commit

---
+++ PortfolioSimulation/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PortfolioSimulation
+//
+//  Created by Falko Schumann on 31.05.20.
+//  Copyright © 2020 Falko Schumann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
